db88063232544277915809101ba9e0d7603d1e7c
Mohamed Mousa <moimousa03@gmail.com>
2026-03-03 09:46:58 +0100

    Completato struttura di Hotel e Stanza

--- src/it/prova/models/Hotel.java
+++ src/it/prova/models/Hotel.java
@@ -1,4 +1,15 @@
 package it.prova.models;
 
 public class Hotel {
+
+    private String ragioneSociale;
+    private int stelle;
+
+
+    public Hotel(){}
+
+    public double calcolaConto(Stanza input){}
+
+
+
 }
--- src/it/prova/models/Stanza.java
+++ src/it/prova/models/Stanza.java
@@ -1,4 +1,10 @@
 package it.prova.models;
 
 public class Stanza {
+
+    private int numeroStanze;
+    private double quantoAPersona;
+
+
+    public Stanza(){}
 }
